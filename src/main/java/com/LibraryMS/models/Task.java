f5e361524178b5d63b2c4807c4af63a1fc268a27
rayyanalhourani <rayyan.alhourani02@gmail.com>
2023-02-18 23:54:32 +0300

    create tables in database

--- src/main/java/com/LibraryMS/models/Task.java
+++ src/main/java/com/LibraryMS/models/Task.java
@@ -1,14 +1,12 @@
 package com.LibraryMS.models;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
 @NoArgsConstructor
