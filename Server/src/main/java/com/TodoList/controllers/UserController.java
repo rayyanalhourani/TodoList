6fd8f8b65609a5ffb102825a9237a5bb8172db57
rayyanalhourani <rayyan.alhourani02@gmail.com>
2023-03-19 08:50:47 +0300

    add origins

--- Server/src/main/java/com/TodoList/controllers/UserController.java
+++ Server/src/main/java/com/TodoList/controllers/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://127.0.0.1:5173/")
 public class UserController {
 
     @Autowired
